c01a727649f80174f09d597f8f639a588b065ac6
Beulaieon <brian_salangsang@dlsu.edu.ph>
2025-03-01 17:22:57 +0800

    use dashboard2.css

--- dashboard-inventory.php
+++ dashboard-inventory.php
@@ -330,7 +330,7 @@ $franchisees = isset($_POST["franchisees"]) ? array_map(fn($f) => $franchiseMap[
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/navbar.css">
-    <link rel="stylesheet" href="assets/css/dashboard.css">
+    <link rel="stylesheet" href="assets/css/dashboard2.css">
     <link rel="stylesheet" href="assets/css/inventory-dashboard.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     
