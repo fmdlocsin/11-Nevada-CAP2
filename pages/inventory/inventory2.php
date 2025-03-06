5c87b4986eff7fa002b1b2331ef62ecd902978f1
jldlp <julia_dalipe@dlsu.edu.ph>
2025-03-07 00:45:55 +0800

    fixed inventory title

--- pages/inventory/inventory2.php
+++ pages/inventory/inventory2.php
@@ -12,7 +12,7 @@ include("../../phpscripts/check-login.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Total Employees</title>
+    <title>Inventory</title>
 
     <!-- ========= CSS ========= -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
